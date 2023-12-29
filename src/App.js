aaea9dd0dce044528bfee0097683ccdb00daac7e
Thamilanpu <sthamil1016@gmail.com>
2023-12-29 12:23:43 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import Button from './Button';
+import "./assets/vendor/bootstrap/css/bootstrap.min.css"
+import './assets/css/style.css';
+import Header from './Header';
+import Body from './Body';
+import Footer from './Footer';
+
+function App() {
+  return (
+    <div className="App">
+
+{/* Hello Thamilanpu */}
+      {/* <Button/> */}
+      <Header/>
+      <Body/>
+      <Footer/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Button.js
@@ -0,0 +1,22 @@
+function Button() {
+    return (
+   <div class="Button">
+       <button>
+       Click Button
+      </button>
+      <Text></Text>
+   </div>
+    );
+  }
+
+  function Text() {
+    return (
+   <div class="Text">
+    Hi Thamilanpu
+   </div>
+    );
+  }
+  
+  export default Button;
+
+
